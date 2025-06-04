f7ee72b445a4c1a9ab5f76a29d7257276a640eea
vocuongthinh2@gmail.com <vocuongthinh2@gmail.com>
2025-06-04 11:15:28 +0700

    create servlet login and get parameter from request

---
+++ src/java/thinhthutha/controller/LoginServlet.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package thinhthutha.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class LoginServlet extends HttpServlet {
+
+    //tao 2 cai hang so nay de chuyen page 
+    //hang so phai viet hoa khong thay t dam
+    private final String SEARCH_PAGE = "search.html";
+    private final String INVALID_PAGE = "invalid.html";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        //cai printwriter dem ra khoi ngoac tron ()
+        PrintWriter out = response.getWriter();
+        //get toan bo information user
+        //quy dung cai nao thi lay cai do
+        String button = request.getParameter("btAction");
+        try {
+            //1. check xem user co bam submit hong 
+            //co thi bat dau get parameter tu 2 o user va password
+            if (button.equals("Login")) {
+                String username = request.getParameter("txtUsername");
+                String password = request.getParameter("txtPassword");
+                //2. Sau khi co parameter thi Controller bat dau call method Model
+                //2.1. New DAO object 
+                
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
